b15d0289ce99a00302c15bc16726f42f7ede436c
soo <sklim.dev@gmail.com>
2022-06-14 16:57:12 +0900

    feat: login 성공 시 username local storage에 저장 기능 구현

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 const loginForm = document.querySelector(".login-form");
+const greeting = document.querySelector(".greeting");
+
 
 function onSubmitLoginForm(event) {
+	const username = document.querySelector(".login-form input").value;
+
 	event.preventDefault();
-	const username = loginForm.querySelector("input").value;
-	alert(`Hello ${username}`);
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	greeting.innerText = `Hello! My friend, ${username}`;
+	greeting.classList.remove(HIDDEN_CLASSNAME);
+
+	localStorage.setItem(USERNAME_KEY, username);
 }
 
-loginForm.addEventListener("submit", onSubmitLoginForm);
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if (saveUsername === null) {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener("submit", onSubmitLoginForm);
+} else {
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	greeting.classList.remove(HIDDEN_CLASSNAME);
+	greeting.innerText = `Hello! My friend, ${username}`;
+}
