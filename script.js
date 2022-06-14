c1b3d8ba709099411ba511465abc0560e6396002
soo <sklim.dev@gmail.com>
2022-06-14 17:45:19 +0900

    refactor: 중복 코드(greeting class 관련 코드) 제거 및 함수로 구현, localStorage 데이터 저장 및 addEventListener 블록 스코프로 변경

--- script.js
+++ script.js
@@ -2,27 +2,30 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 const loginForm = document.querySelector(".login-form");
-const greeting = document.querySelector(".greeting");
 
+function displayGreetings(username) {
+	const greeting = document.querySelector(".greeting");
 
-function onSubmitLoginForm(event) {
-	const username = document.querySelector(".login-form input").value;
-
-	event.preventDefault();
-	loginForm.classList.add(HIDDEN_CLASSNAME);
 	greeting.innerText = `Hello! My friend, ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+function onSubmitLoginForm(event) {
+	event.preventDefault();
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+
+	const username = document.querySelector(".login-form input").value;
+	displayGreetings(username);
 	localStorage.setItem(USERNAME_KEY, username);
 }
 
-const saveUsername = localStorage.getItem(USERNAME_KEY);
+{
+	const saveUsername = localStorage.getItem(USERNAME_KEY);
 
-if (saveUsername === null) {
-	loginForm.classList.remove(HIDDEN_CLASSNAME);
-	loginForm.addEventListener("submit", onSubmitLoginForm);
-} else {
-	loginForm.classList.add(HIDDEN_CLASSNAME);
-	greeting.classList.remove(HIDDEN_CLASSNAME);
-	greeting.innerText = `Hello! My friend, ${username}`;
+	if (saveUsername === null) {
+		loginForm.classList.remove(HIDDEN_CLASSNAME);
+		loginForm.addEventListener("submit", onSubmitLoginForm);
+	} else {
+		displayGreetings(saveUsername);
+	}
 }
